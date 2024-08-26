96192aa4911b44d0e3cdafadf866d2c386163035
JonathanParra467 <jparra1786@students.mchenry.edu>
2024-08-25 19:18:05 -0500

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 34
+print("Literal:", x)
+
+# Integer
+y = -19
+print("Integer:", y)
+
+# Float
+z = 67.5
+print("Float:", z)
+
+# String
+name = "mike"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
